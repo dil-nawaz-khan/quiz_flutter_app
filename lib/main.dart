21b4dc20bde69b4cb16fcd6e116a175760f66a98
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-04-27 10:31:03 +0500

    implement StartScreen widget and update main function for improved UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,61 @@
 import 'package:flutter/material.dart';
 
+import 'package:quiz_flutter_app/start_screen.dart';
+
+// void main() {
+//   return runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.deepPurple,
+//         body: Center(
+//           child: Column(
+//             spacing: 20.0,
+//             children: [
+//               SizedBox(height: 130.0),
+//               Image.asset('assets/images/quiz-logo.png', width: 280.0),
+//               SizedBox(height: 20.0),
+//               Text(
+//                 'Learn Flutter the fun way!',
+//                 style: TextStyle(color: Colors.white, fontSize: 20.0),
+//               ),
+//               OutlinedButton(
+//                 style: OutlinedButton.styleFrom(
+//                   foregroundColor: Colors.white,
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(5.0),
+//                   ),
+//                   side: BorderSide(color: Colors.white60),
+//                   textStyle: TextStyle(fontSize: 16.0),
+//                   padding: EdgeInsets.symmetric(
+//                     vertical: 8.0,
+//                     horizontal: 15.0,
+//                   ),
+//                 ),
+//                 onPressed: () {},
+//                 child: Text('Start Quiz'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+// Max Solution
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Column(
-            spacing: 20.0,
-            children: [
-              SizedBox(height: 130.0),
-              Image.asset('assets/images/quiz-logo.png', width: 280.0),
-              SizedBox(height: 20.0),
-              Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-              ),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  side: BorderSide(color: Colors.white60),
-                  textStyle: TextStyle(fontSize: 16.0),
-                  padding: EdgeInsets.symmetric(
-                    vertical: 8.0,
-                    horizontal: 15.0,
-                  ),
-                ),
-                onPressed: () {},
-                child: Text('Start Quiz'),
-              ),
-            ],
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
+          child: StartScreen(),
         ),
       ),
     ),
---
+++ lib/start_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Opacity(
+          //   opacity: .1,
+          //   child: Image.asset('assets/images/quiz-logo.png', width: 300.0),
+          // ), //performance issue
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300.0,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          SizedBox(height: 80.0),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24.0),
+          ),
+          SizedBox(height: 30.0),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: Text('Start Quiz!'),
+            icon: Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
+    );
+  }
+}
